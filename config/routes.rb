314ed87199404b6f99d586828a53f8438c160864
Nehemia Telusma <telusma.nehemia@gmail.com>
2022-03-31 16:51:56 +0200

    le chat fonctionne (la base)

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'home/index' , to: 'home#index'
+
+  root 'home#index'
+  resources :messages
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
